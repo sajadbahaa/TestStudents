3bbb46df628b7b18fbdf21a73338cfcfecb7fb26
Sajad Bahaa <sgad256@gmail.com>
2025-09-15 19:35:56 +0300

    layer resposiable for Controllers

--- WebAPI/WebAPI/obj/Debug/net8.0/WebAPI.AssemblyInfo.cs
+++ WebAPI/WebAPI/obj/Debug/net8.0/WebAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be64209f652e9b60b22233be248ef9c5ec00fada")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93d9be582e9e32316a2cedc7b180477a8b4686e3")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
