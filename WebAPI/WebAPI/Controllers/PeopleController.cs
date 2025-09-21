c23da2a97b4ebf5934c8e0e1e055314e6243bd8f
Sajad Bahaa <sgad256@gmail.com>
2025-09-22 02:23:30 +0300

    add People Controller

---
+++ WebAPI/WebAPI/Controllers/PeopleController.cs
@@ -0,0 +1,66 @@
+﻿using BsLayer.Services;
+using Dtos.PeopleDtos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PeopleController : ControllerBase
+    {
+        private readonly PeopleService _peopleService;
+
+        public PeopleController(PeopleService peopleService)
+        {
+            _peopleService = peopleService;
+        }
+
+        // GET: api/People
+        [HttpGet("GetAllPeopleAsync")]
+        public async Task<ActionResult<List<findPeopleDtos>>> GetAllPeopleAsync()
+        {
+            var people = await _peopleService.GetAllPeopleAsync();
+            if (people == null || people.Count == 0)
+                return NotFound("لا يوجد أشخاص.");
+            return Ok(people);
+        }
+
+        // GET: api/People/5
+        [HttpGet("GetPersonByIdAsync/{id}")]
+        public async Task<ActionResult<findPeopleDtos>> GetPersonByIdAsync(int id)
+        {
+            var person = await _peopleService.GetByIDAsync(id);
+            if (person == null)
+                return NotFound("الشخص غير موجود.");
+            return Ok(person);
+        }
+
+        // POST: api/People
+        [HttpPost("AddPersonAsync")]
+        public async Task<ActionResult<int>> AddPersonAsync([FromBody] addPeopleDtos dto)
+        {
+            int id = await _peopleService.AddAsync(dto);
+            return id==0?BadRequest():Ok($"Person ID : {id}");
+        }
+
+        // PUT: api/People/5
+        [HttpPut("UpdatePersonAsync")]
+        public async Task<ActionResult> UpdatePersonAsync([FromBody] updatePeopleDtos dto)
+        {
+            bool result = await _peopleService.UpdateAsync(dto);
+            if (!result)
+                return NotFound("الشخص غير موجود للتحديث.");
+            return NoContent();
+        }
+
+        // DELETE: api/People/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeletePersonAsync(int id)
+        {
+            var result = await _peopleService.DeleteAsync(id);
+            if (!result)
+                return NotFound("الشخص غير موجود للحذف.");
+            return NoContent();
+        }
+    }
+}
--- WebAPI/WebAPI/obj/Debug/net8.0/WebAPI.AssemblyInfo.cs
+++ WebAPI/WebAPI/obj/Debug/net8.0/WebAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5bd5d084afdea462cbd4a75376f99642ccb79acb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+62ae12453069dbadf8c9027921b82a2f1c80310e")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
